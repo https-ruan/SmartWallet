9f2c0ec6bb91426be540db0e436e9adcef02af15
Ruan4S <ruands2002@gmail.com>
2021-12-20 22:35:03 -0300

    Feat(header): estilo header ok

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { RouterModule } from '@angular/router';
 import { I18nModule } from '@app/i18n';
 import { MaterialModule } from '@app/material.module';
-import { ShellComponent } from './shell.component';
+import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from './header/header.component';
+import { ShellComponent } from './shell.component';
 
 @NgModule({
   imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, I18nModule, RouterModule],
